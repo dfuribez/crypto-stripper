49a6384aa454cbe4bfbb484d0da15fd74a0e40c3
Diego <duribe>
2025-04-02 13:13:42 -0500

    add utils

--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -1,7 +1,12 @@
+import burp.api.montoya.http.handler.HttpRequestToBeSent;
 import burp.api.montoya.http.message.HttpHeader;
-import burp.api.montoya.http.message.params.ParsedHttpParameter;
+import burp.api.montoya.http.message.params.HttpParameterType;
+import burp.api.montoya.http.message.requests.HttpRequest;
+import com.google.gson.Gson;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Utils {
@@ -16,4 +21,66 @@ public class Utils {
 
     return o;
   }
+
+  public static HashMap<String, String> prepareForExecutor(
+      HttpRequest request
+  ) {
+    HashMap<String, String> result = new HashMap<String, String>();
+
+    String body = request.bodyToString();
+    String headers = new Gson().toJson(
+        burpListToArray(request.headers()));
+    String urlParameters = new Gson().toJson(
+        burpListToArray(request.parameters(HttpParameterType.URL)));
+
+    result.put("body", body);
+    result.put("headers", headers);
+    result.put("urlParameters", urlParameters);
+
+    return result;
+  }
+
+  public static boolean checkFileExists(String path) {
+
+    if (path == null || path.isEmpty()) {
+      return false;
+    }
+
+    File file = new File(path);
+
+    return file.isFile();
+  }
+
+  public static List<HttpHeader> listToHttpHeaders(List<String> headersList) {
+    List<HttpHeader> headers = new ArrayList<HttpHeader>();
+
+    if (headersList == null || headersList.isEmpty()) {
+      return  headers;
+    }
+
+    for (String h : headersList) {
+      headers.add(HttpHeader.httpHeader(h));
+    }
+
+    return headers;
+  }
+
+
+  public static HttpRequest executorToHttp(HttpRequest request, ExecutorResponse output) {
+    HttpRequest modified = HttpRequest.httpRequest();
+
+    modified = modified.withService(request.httpService());
+    modified = modified.withPath(request.path());
+    modified = modified.withAddedHeaders(listToHttpHeaders(output.getHeaders()));
+
+    if (output.getError() != null && !output.getError().isEmpty()) {
+      modified = modified.withHeader(Constants.STRIPPER_HEADER, "error");
+      modified = modified.withBody(output.getError());
+      return modified;
+    }
+
+    modified = modified.withBody(output.getBody());
+    return modified;
+  }
+
 }

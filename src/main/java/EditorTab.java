d655d65213f1056e451cf28f26beb22946628602
Diego <duribe>
2025-04-22 21:45:09 -0500

    use JTextAreas instead of burp's RawEditors

--- src/main/java/EditorTab.java
+++ src/main/java/EditorTab.java
@@ -4,7 +4,6 @@ import burp.api.montoya.http.message.HttpRequestResponse;
 import burp.api.montoya.http.message.requests.HttpRequest;
 import burp.api.montoya.http.message.responses.HttpResponse;
 import burp.api.montoya.ui.editor.RawEditor;
-import jdk.jshell.execution.Util;
 
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
@@ -23,11 +22,11 @@ public class EditorTab {
   private JPanel stdErrPanel;
   private JButton testEncryptionButton;
   private JPanel buttonsPanel;
+  private JTextArea stdOutTextArea;
+  private JTextArea stdErrTextArea;
 
   MontoyaApi api;
   RawEditor contentEditor;
-  RawEditor stdErrEditor;
-  RawEditor stdOutEditor;
 
   boolean isRequest;
 
@@ -40,17 +39,12 @@ public class EditorTab {
   ) {
     this.isRequest = isRequest;
     this.api = api;
-    this.commandPanel.setBorder(new TitledBorder("Command:"));
-    this.stdErrPanel.setBorder(new TitledBorder("stderr:"));
-    this.stdOutPanel.setBorder(new TitledBorder("stdout:"));
+    commandPanel.setBorder(new TitledBorder("Command:"));
+    stdErrPanel.setBorder(new TitledBorder("stderr:"));
+    stdOutPanel.setBorder(new TitledBorder("stdout:"));
 
-    this.contentEditor = api.userInterface().createRawEditor();
-    this.stdErrEditor = api.userInterface().createRawEditor();
-    this.stdOutEditor = api.userInterface().createRawEditor();
-
-    this.stdOutEditor.setEditable(false);
-    this.stdErrEditor.setEditable(false);
-    this.contentEditor.setEditable(false);
+    contentEditor = api.userInterface().createRawEditor();
+    contentEditor.setEditable(false);
 
     GridBagConstraints gbc = new GridBagConstraints();
     gbc.gridx = 0;
@@ -58,9 +52,8 @@ public class EditorTab {
     gbc.fill = GridBagConstraints.BOTH;
     gbc.weightx = 1.0;
     gbc.weighty = 1.0;
-    this.commandPanel.add(this.contentEditor.uiComponent(), gbc);
-    this.stdErrPanel.add(this.stdErrEditor.uiComponent(), gbc);
-    this.stdOutPanel.add(this.stdOutEditor.uiComponent(), gbc);
+
+    commandPanel.add(contentEditor.uiComponent(), gbc);
 
     testEncryptionButton.addActionListener(new ActionListener() {
       @Override
@@ -86,23 +79,22 @@ public class EditorTab {
     HashMap<String, String> prepared = new HashMap<>();
     String source;
     String stdOutBox = "";
-    String stdErrBox = "";
 
-    if (this.isRequest) {
+    if (isRequest) {
       prepared = Utils.prepareRequestForExecutor(
-              this.requestResponse.request(), 0);
+              requestResponse.request(), 0);
       source = "request";
 
     } else {
       String url =
-          Utils.removeQueryFromUrl(this.requestResponse.request().url());
+          Utils.removeQueryFromUrl(requestResponse.request().url());
       prepared = Utils.prepareResponseForExecutor(
-          this.requestResponse.response(), url, 0);
+          requestResponse.response(), url, 0);
       source = "response";
     }
 
     ExecutorResponse executed = Executor.execute(
-        this.api,
+        api,
         action,
         source,
         prepared
@@ -111,7 +103,7 @@ public class EditorTab {
 
     if (isRequest) {
       HttpRequest request =
-          Utils.executorToHttp(this.requestResponse.request(), executed);
+          Utils.executorToHttp(requestResponse.request(), executed);
 
       stdOutBox = executed.getError();
 
@@ -120,7 +112,7 @@ public class EditorTab {
       }
     } else {
       HttpResponse response =
-          Utils.executorToHttpResponse(this.requestResponse.response(), executed);
+          Utils.executorToHttpResponse(requestResponse.response(), executed);
       stdOutBox = executed.getError();
 
       if (stdOutBox.isBlank()) {
@@ -128,12 +120,11 @@ public class EditorTab {
       }
     }
 
-    this.stdOutEditor.setContents(ByteArray.byteArray(stdOutBox));
-    this.stdErrEditor.setContents(ByteArray.byteArray(
-        executed.getStdErr().getBytes()));
+    stdOutTextArea.setText(stdOutBox);
+    stdErrTextArea.setText(executed.getStdErr());
   }
 
   public void setCommand(ByteArray content) {
-    this.contentEditor.setContents(content);
+    contentEditor.setContents(content);
   }
 }

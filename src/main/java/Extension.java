96cc14063a74a7ca3d13975a704b3cb878d59563
Diego <duribe>
2025-05-01 15:49:42 -0500

    add initial message

--- src/main/java/Extension.java
+++ src/main/java/Extension.java
@@ -13,6 +13,13 @@ public class Extension implements BurpExtension {
 
     MainTab tab = new MainTab(api);
 
+    api.logging().logToOutput("""
+        Crypto Stripper by Diego Uribe
+        Version: v0.5-alpha
+        Github: https://github.com/dfuribez/crypto-stripper
+        Documentation: https://github.com/dfuribez/crypto-stripper/wiki
+        """);
+
     api.userInterface().registerSuiteTab("Stripper", tab.panel1);
     api.userInterface().registerContextMenuItemsProvider(
         new MyContextMenus(api, tab));

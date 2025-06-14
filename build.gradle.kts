df23884830988741fb0acbc646a95f469c221804
Diego <duribe>
2025-06-14 13:32:17 -0500

    add gson dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ repositories {
 
 dependencies {
     compileOnly("net.portswigger.burp.extensions:montoya-api:2025.2")
+    implementation("com.google.code.gson:gson:2.10.1")
 }
 
 tasks.withType<JavaCompile> {

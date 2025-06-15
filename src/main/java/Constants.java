64c428432016e30f083c2e8ba4e40bb3a144fe1f
Diego <duribe>
2025-06-14 20:08:51 -0500

    update templates

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -42,37 +42,44 @@ public class Constants {
 let fs = require("fs");
 
 // Function that performs the decryption
-function decrypt(body, headers, params, statusCode, reasonPhrase, url, messageId) {
+async function decrypt(body, headers, urlParameters, httpMethod, path, statusCode, reasonPhrase, url, messageId) {
     let replaceResponse = false;  // only used in responses
     console.error("only use console.error to debug")
     console.error("the use of console.log will cause the process to fail")
-    return [body, headers, params, statusCode, reasonPhrase, replaceResponse]
+
+    return [body, headers, urlParameters, httpMethod, path, statusCode, reasonPhrase, replaceResponse]
 }
 
 
 // Function that perform encryption
-function encrypt(body, headers, params, statusCode, reasonPhrase, url, messageId) {
-    return [body, headers, params, statusCode, reasonPhrase];
+async function encrypt(body, headers, urlParameters, httpMethod, path, statusCode, reasonPhrase, url, messageId) {
+    console.error("only use console.error to debug")
+    console.error("the use of console.log will cause the process to fail")
+
+    return [body, headers, urlParameters, httpMethod, path, statusCode, reasonPhrase];
 }
 
 
 // DON'T TOUCH THIS
-function printJSON(body, headers, params, statusCode, reasonPhrase, replaceResponse=false) {
+function printJSON(body, headers, urlParameters, httpMethod, path, statusCode, reasonPhrase, replaceResponse=false) {
     console.log(
         Buffer.from(
             JSON.stringify({
                 body: body,
                 headers: headers,
-                urlParameters: params,
+                urlParameters: urlParameters,
                 replaceResponse: replaceResponse,
                 statusCode: statusCode,
-                reasonPhrase: reasonPhrase
+                reasonPhrase: reasonPhrase,
+                httpMethod: httpMethod,
+                path: path
             })
         ).toString("base64")
     )
 }
 
-function main() {
+async function main() {
+    console.error(process.argv[2])
     var jsonData = JSON.parse(fs.readFileSync(process.argv[2]).toString())
 
     var body = jsonData.body
@@ -82,17 +89,21 @@ function main() {
     var messageId = jsonData.messageId
     var statusCode = jsonData.statusCode
     var reasonPhrase = jsonData.reasonPhrase
+    var httpMethod = jsonData.httpMethod
+    var path = jsonData.path
 
     if (jsonData.action == "encrypt") {
-        printJSON(...encrypt(body, headers, urlParameters, statusCode, reasonPhrase, url, messageId))
+        var enc = await encrypt(body, headers, urlParameters, httpMethod, path, statusCode, reasonPhrase, url, messageId)
+        printJSON(...enc)
     } else {
-        printJSON(...decrypt(body, headers, urlParameters, statusCode, reasonPhrase, url, messageId))
+        var dec = await decrypt(body, headers, urlParameters, httpMethod, path, statusCode, reasonPhrase, url, messageId)
+        printJSON(...dec)
     }
 }
 
 main()
 
-      """;
+  """;
 
   public static String PYTHON_TEMPLATE = """
 # Crypto Stripper v0.3
@@ -102,23 +113,24 @@ import base64
 import json
 import sys
 
-def encrypt(body, headers, url_parameters, status_code, reason_phrase, url, messageid):
+def encrypt(body, headers, url_parameters, http_method, path, status_code, reason_phrase, url, messageid):
     # implement your code here
     print("only print to stderr to debug", file=sys.stderr)
     print("the use of print() will cause the process to fail", file=sys.stderr)
-    return body, headers, url_parameters, status_code, reason_phrase
+
+    return body, headers, url_parameters, http_method, path, status_code, reason_phrase
 
 
-def decrypt(body, headers, url_parameters, status_code, reason_phrase, url, messageid):
+def decrypt(body, headers, url_parameters, http_method, path, status_code, reason_phrase, url, messageid):
     # implement your code here
     replace_response = False
     print("only print to stderr to debug", file=sys.stderr)
     print("the use of print() will cause the process to fail", file=sys.stderr)
-    return body, headers, url_parameters, status_code, reason_phrase, replace_response
+    return body, headers, url_parameters, http_method, path, status_code, reason_phrase, replace_response
 
 
 # DON'T MODIFY THIS
-def print_json(body, headers, url_parameters, status_code, reason_phrase, replace_response=False):
+def print_json(body, headers, url_parameters, http_method, path, status_code, reason_phrase, replace_response=False):
     print(
         base64.b64encode(
             json.dumps({
@@ -127,11 +139,14 @@ def print_json(body, headers, url_parameters, status_code, reason_phrase, replac
                 "urlParameters": url_parameters,
                 "replaceResponse": replace_response,
                 "statusCode": status_code,
-                "reasonPhrase": reason_phrase
+                "reasonPhrase": reason_phrase,
+                "httpMethod": http_method,
+                "path": path
                 }
             ).encode("utf8")
         ).decode()
     )
+
 
 with open(sys.argv[1]) as file:
     json_content = json.load(file)
@@ -139,16 +154,19 @@ with open(sys.argv[1]) as file:
     body = json_content["body"]
     headers = json.loads(json_content["headers"])
     url = json_content["url"]
-    urlparameters = json.loads(json_content["urlParameters"])
+    url_parameters = json.loads(json_content["urlParameters"])
     messageid = json_content["messageId"]
     status_code = json_content.get("statusCode", 0)
     reason_phrase = json_content.get("reasonPhrase", "")
+    http_method = json_content.get("httpMethod", "")
+    path = json_content.get("path", "")
 
     if (json_content["action"] == "encrypt"):
-        encrypted = encrypt(body, headers, urlparameters, status_code, reason_phrase, url, messageid)
+        encrypted = encrypt(body, headers, url_parameters, http_method, path, status_code, reason_phrase, url, messageid)
         print_json(*encrypted)
     else:
-        decrypted = decrypt(body, headers, urlparameters, status_code, reason_phrase, url, messageid)
+        decrypted = decrypt(body, headers, url_parameters, http_method, path, status_code, reason_phrase, url, messageid)
         print_json(*decrypted)
-      """;
+
+  """;
 }

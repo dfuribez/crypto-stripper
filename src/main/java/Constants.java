2582dd149093909bc2f08721b769cc9468554135
Diego <duribe>
2026-01-18 21:54:58 -0500

    update version

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -29,7 +29,7 @@ public class Constants {
 
   public static String VERSION = """
       Crypto Stripper by Diego Uribe
-      Version: v0.11-alpha
+      Version: v0.12-alpha
       Github: https://github.com/dfuribez/crypto-stripper
       Examples: https://github.com/dfuribez/crypto-stripper/wiki
       Playground: https://github.com/dfuribez/crypto-sripper-playground

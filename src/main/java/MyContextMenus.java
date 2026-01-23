dfda6c49731960e1f27f00ce995789f7878c3b67
Diego <duribe>
2026-01-22 19:39:23 -0500

    refactor payload window

--- src/main/java/MyContextMenus.java
+++ src/main/java/MyContextMenus.java
@@ -2,6 +2,8 @@ import burp.api.montoya.MontoyaApi;
 import burp.api.montoya.core.ByteArray;
 import burp.api.montoya.core.ToolType;
 import burp.api.montoya.http.message.HttpRequestResponse;
+import burp.api.montoya.http.message.params.HttpParameter;
+import burp.api.montoya.http.message.params.HttpParameterType;
 import burp.api.montoya.http.message.requests.HttpRequest;
 import burp.api.montoya.persistence.PersistedList;
 import burp.api.montoya.ui.contextmenu.ContextMenuEvent;
@@ -15,9 +17,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.*;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -136,18 +136,28 @@ public class MyContextMenus  implements ContextMenuItemsProvider {
       insertPayloadMenu = new JMenuItem("Insert payload");
       insertPayloadMenu.addActionListener(l -> {
         int cursorPosition = editorHttpRequestResponse.caretPosition();
-        byte[] content = editorHttpRequestResponse.requestResponse().request().toByteArray().getBytes();
+        byte[] content = editorHttpRequestResponse
+            .requestResponse().request().toByteArray().getBytes();
 
+        HttpRequest request =
+            editorHttpRequestResponse.requestResponse().request();
+
+        insertDialog.setParameters(request.parameters());
         insertDialog.pack();
         insertDialog.setVisible(true);
 
+        String selectedComboParameter = insertDialog.getSelectedParameter();
+        String[] split = selectedComboParameter.split(" - ", 2);
+
+        String selectedParameter = split[1];
         byte[] toInsert = InsertDialog.selectedText;
 
         if (toInsert == null) { return; }
 
         if (insertDialog.base64RadioButton.isSelected()) {
           toInsert = montoyaApi.utilities().base64Utils().encodeToString(
-              ByteArray.byteArray(toInsert)).getBytes();
+              ByteArray.byteArray(toInsert)
+          ).getBytes();
         }
 
         if (insertDialog.URLEncodeRadioButton.isSelected()) {
@@ -155,6 +165,20 @@ public class MyContextMenus  implements ContextMenuItemsProvider {
               new String(toInsert, StandardCharsets.UTF_8),
               StandardCharsets.UTF_8
           ).getBytes(StandardCharsets.UTF_8);
+        }
+
+        if (!selectedParameter.equals("SELECTION POINT")) {
+          HttpParameterType selectedParameterType = HttpParameterType.valueOf(split[0]);
+          String value = ByteArray.byteArray(toInsert).toString();
+
+          HttpParameter parameter = HttpParameter.parameter(
+              selectedParameter,
+              value,
+              selectedParameterType
+          );
+
+          editorHttpRequestResponse.setRequest(request.withParameter(parameter));
+          return;
         }
 
         byte[] s = Arrays.copyOfRange(content, 0, cursorPosition);
@@ -167,7 +191,8 @@ public class MyContextMenus  implements ContextMenuItemsProvider {
         System.arraycopy(f, 0, nRequest, s.length + toInsert.length, f.length);
 
         editorHttpRequestResponse.setRequest(
-            HttpRequest.httpRequest(ByteArray.byteArray(nRequest)));
+            HttpRequest.httpRequest(ByteArray.byteArray(nRequest))
+        );
       });
     }
 
@@ -189,31 +214,37 @@ public class MyContextMenus  implements ContextMenuItemsProvider {
       if (Utils.isUrlInScope(url, scope.get("blacklist"))) {
         stripperBlackListMenu = new JMenuItem("Remove endpoint from blacklist");
         stripperBlackListMenu.addActionListener(
-            l -> this.updateStripperScope("blacklist", "remove", url));
+            l -> this.updateStripperScope("blacklist", "remove", url)
+        );
       } else {
         stripperBlackListMenu = new JMenuItem("Add endpoint to blacklist");
         stripperBlackListMenu.addActionListener(
-            l -> this.updateStripperScope("blacklist", "add", url));
+            l -> this.updateStripperScope("blacklist", "add", url)
+        );
       }
 
       if (Utils.isUrlInScope(url, scope.get("force"))) {
         stripperForceMenu = new JMenuItem("Do not force interception");
         stripperForceMenu.addActionListener(
-            l -> this.updateStripperScope("force", "remove", url));
+            l -> this.updateStripperScope("force", "remove", url)
+        );
       } else {
         stripperForceMenu = new JMenuItem("Force interception this endpoint");
         stripperForceMenu.addActionListener(
-            l -> this.updateStripperScope("force", "add", url));
+            l -> this.updateStripperScope("force", "add", url)
+        );
       }
 
       if (montoyaApi.scope().isInScope(url)) {
         burpScopeMenu = new JMenuItem("Exclude URL from Burp's scope");
         burpScopeMenu.addActionListener(
-            l -> montoyaApi.scope().excludeFromScope(Utils.removePathFromURL(url)));
+            l -> montoyaApi.scope().excludeFromScope(Utils.removePathFromURL(url))
+        );
       } else {
         burpScopeMenu = new JMenuItem("Include URL to Burp's scope");
         burpScopeMenu.addActionListener(
-            l -> montoyaApi.scope().includeInScope(Utils.removePathFromURL(url)));
+            l -> montoyaApi.scope().includeInScope(Utils.removePathFromURL(url))
+        );
       }
 
       addToPassThroughMenu = new JMenuItem("Add host to Burp's pass through");
@@ -221,11 +252,10 @@ public class MyContextMenus  implements ContextMenuItemsProvider {
       addToPassThroughMenu.addActionListener(l -> {
         String host = finalRequestResponse.request().httpService().host()
             .replace(".", "\\\\.");
+
         int port = finalRequestResponse.request().httpService().port();
         String pass = String.format(Constants.PASS_THROUGH, host, port);
-
         String current = montoyaApi.burpSuite().exportProjectOptionsAsJson();
-
         JsonObject root = JsonParser.parseString(current).getAsJsonObject();
 
         JsonArray rules = root

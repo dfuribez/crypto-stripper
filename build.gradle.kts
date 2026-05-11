fb510b37257e9a6dddd13fdd33a8d921a9779b93
Diego Uribe <duribe>
2026-05-10 19:03:24 -0500

    remove gson

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,6 @@ repositories {
 
 dependencies {
     compileOnly("net.portswigger.burp.extensions:montoya-api:2025.2")
-    implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.miglayout:miglayout-swing:11.3")
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
